01ae1a9fcdfea3c24dc6797cb6135055074470ae
ipe <izaki.yb1998@gmail.com>
2020-04-22 09:13:20 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -20,7 +22,14 @@ Rails.application.routes.draw do
   get '/signup', to:'users#new'
 
   post '/signup',  to: 'users#create'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   # usersでRestが使えるようにする
   resources :users
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
